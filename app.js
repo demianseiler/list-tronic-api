e514a77e134f1804078949fa6ea0d8b2beb5b4fd
Demian <demian@presstronic.com>
2019-08-10 00:36:41 -0400

    Adding testing dependencies

--- app.js
+++ app.js
@@ -7,8 +7,7 @@
  * Project Module Imports
  * */
 const pkg = require('./package.json');
-const fs    = require('fs');
-const nconf = require('nconf');
+const dotenv = require('dotenv').config();
 
 /**
  * 3rd Party Module Imports
@@ -18,9 +17,17 @@ const express = require('express');
 // Create express 'app' object
 const app = express();
 
+// Setup application port
+const DEFAULT_APP_PORT = 3000;
 
+const APP_PORT = ( process.env.API_PORT ||  DEFAULT_APP_PORT);
+
+// Begin routes
+app.get('/', function(req, res) {
+    res.send('Hello World');
+});
 
 
 
 // Start 'app' and listen on for requests
-app.listen()
+app.listen(APP_PORT, () => console.log(`Chat Tronic API application is listening on port ${APP_PORT}`));
